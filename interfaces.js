7deffaf60afacfb1c4d1fe5858f3146cf137026b
user2170867 <dasamaksimova08@mail.ru>
2026-05-27 15:22:22 +0300

    interfaces.js

---
+++ interfaces.js
@@ -0,0 +1,5 @@
+export class InputOutput {
+    getA() { throw new Error('Not implemented'); }
+    getB() { throw new Error('Not implemented'); }
+    showResult(value) { throw new Error('Not implemented'); }
+}
